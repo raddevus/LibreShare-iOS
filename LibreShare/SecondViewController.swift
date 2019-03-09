e3e0a5145f1d3487ed30fdac3a717d34208ac010
raddevus <1radeutsch1@gmail.com>
2019-03-08 19:03:14 -0500

    Initial Commit

---
+++ LibreShare/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LibreShare
+//
+//  Created by roger deutsch on 3/8/19.
+//  Copyright © 2019 Roger Deutsch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
