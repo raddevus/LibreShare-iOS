76333ffbf42ef79355591ebf571bb0669a4388e4
raddevus <1radeutsch1@gmail.com>
2019-03-09 18:01:35 -0500

    Yes! It wrote to Firebase with no problem. First test is success.

--- LibreShare/FirstViewController.swift
+++ LibreShare/FirstViewController.swift
@@ -23,8 +23,8 @@ class FirstViewController: UIViewController {
 
     @IBAction func getEntries_click(_ sender : UIButton){
         //FirebaseApp.initialize()
-        //ref.child(
-        
+        //ref.  child("AB8763B964DD92B42F4566BE1445E20E8088EF4A").child("52223097E36341DEE340D7D49AF7ED257C13687A")
+        ref.child("message").setValue("from Apple!")
         titleLabel.text = "I have changed!"
     }
 
