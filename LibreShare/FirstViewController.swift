2136219660a11f60e7fda548a27cffd13ef57e63
raddevus <1radeutsch1@gmail.com>
2019-03-09 21:25:01 -0500

    Small test which adds current time to the remote firebase message and to the main view. Shows seconds so you can see it is updating often.

--- LibreShare/FirstViewController.swift
+++ LibreShare/FirstViewController.swift
@@ -24,8 +24,15 @@ class FirstViewController: UIViewController {
     @IBAction func getEntries_click(_ sender : UIButton){
         //FirebaseApp.initialize()
         //ref.  child("AB8763B964DD92B42F4566BE1445E20E8088EF4A").child("52223097E36341DEE340D7D49AF7ED257C13687A")
-        ref.child("message").setValue("from Apple!")
-        titleLabel.text = "I have changed!"
+        
+        let date = Date()
+        let dateFormatter : DateFormatter = DateFormatter()
+       
+        dateFormatter.dateFormat = "yyyy-MMM-dd HH:mm:ss"
+        let dateString = dateFormatter.string(from: date)
+        
+        ref.child("message").setValue("from Apple! - " + dateString)
+        titleLabel.text = dateString
     }
 
 }
